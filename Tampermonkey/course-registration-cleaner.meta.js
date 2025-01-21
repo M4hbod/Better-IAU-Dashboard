fe407cded6e3db8683746294b6bb3cceb3a0f029
Mahbod <74229780+M4hbod@users.noreply.github.com>
2025-01-21 16:28:42 +0330

    Create course-registration-cleaner.meta.js

---
+++ Tampermonkey/course-registration-cleaner.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Course Registration Cleaner
+// @namespace    http://github.com/M4hbod
+// @version      1.0
+// @description  Filters out classes that are fully booked or exclusively for members of the opposite sex, helping you stay focused.
+// @author       http://github.com/M4hbod
+// @match        https://stdn.iau.ir/Student/registerationAction.do
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=iau.ir
+// @grant        GM_registerMenuCommand
+// @grant        GM_setValue
+// @grant        GM_getValue
+// ==/UserScript==
